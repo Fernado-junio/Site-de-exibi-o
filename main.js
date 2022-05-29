3f23909c74741d3a10d1708b32c3fd99d66a9731
Fernando <junio132sj@gmail.com>
2022-05-29 04:54:50 -0300

    fix

---
+++ main.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+
+
+const port = process.env.PORT || 80
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}` );
+});
