06669ce3a1143cc6d52f14372fc1b99e1ad845b5
Fernando <junio132sj@gmail.com>
2022-06-04 05:09:45 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 
+var abrir = document.getElementById('abrir')
+var fechar = document.getElementById('fechar')
+var clicks = 1
+function categorias(){
+    clicks++
+    if (clicks == 0) {
+        abrir.style.display = 'none'
+        fechar.style.display = 'inline'
+    }
+    if (clicks == 1) {
+        abrir.style.display = 'inline'
+        fechar.style.display = 'none'
+    }
+    if (clicks == 2) {
+        abrir.style.display = 'none'
+        fechar.style.display = 'inline'
+        clicks = 0
+    }
+}
+
+
+
+
+
+
+
 function idade(dia, mes, ano) {
     let idadeAno = new Date().getFullYear() - ano;
     var Mes = new Date().getMonth() + 1
