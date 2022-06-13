2f80e6af0861d55fe8cf0496882e66d35996a492
Fernando <junio132sj@gmail.com>
2022-06-13 02:14:08 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -4,22 +4,7 @@ function not() {
 }
 
 
-//sobre
 
-
-function idade(dia, mes, ano) {
-    let idadeAno = new Date().getFullYear() - ano;
-    var Mes = new Date().getMonth() + 1
-    var Dia = new Date().getDate()
-    if (mes > Mes  || mes == Mes && dia > Dia) {
-        return idadeAno - 1
-    }
-
-    if (mes == Mes && dia < Dia || mes == Mes && dia == Dia || mes < Mes) {
-        return idadeAno
-    }
-}
-document.getElementById('idade').innerText = idade(30,09,2005)
 
 
 var menuButton = document.getElementById('menuButton')
@@ -76,7 +61,24 @@ function categorias(){
         clicks = 0
     }
 }
+//sobre
 
+
+function sobre() {
+    function idade(dia, mes, ano) {
+        let idadeAno = new Date().getFullYear() - ano;
+        var Mes = new Date().getMonth() + 1
+        var Dia = new Date().getDate()
+        if (mes > Mes  || mes == Mes && dia > Dia) {
+            return idadeAno - 1
+        }
+    
+        if (mes == Mes && dia < Dia || mes == Mes && dia == Dia || mes < Mes) {
+            return idadeAno
+        }
+    }
+    document.getElementById('idade').innerHTML = idade(30,09,2005)
+}
 
 
 
